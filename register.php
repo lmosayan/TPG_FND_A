a6108c9bc2c3e3b2bf7576a78dc246a430b3c826
lmosayan <154394980+lmosayan@users.noreply.github.com>
2024-01-07 00:40:27 +0800

    Add files via upload

---
+++ register.php
@@ -0,0 +1,30 @@
+<?php
+    $email = $_GET['remail'];
+    $pass = $_GET['rpass'];
+    $uname = $_GET['rtext'];
+
+    // Database Connection Code
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "webpage";
+
+    $con = mysqli_connect($servername, $username, $password, $dbname);
+
+    if (!$con) {
+        die("Error: " . mysqli_connect_error());
+    }
+
+    // SQL query for inserting data
+    $sql = "INSERT INTO `webpage`(`Username`, `Email`, `Password`) VALUES ('$uname', '$email', '$pass')";
+
+    // Execute the query
+    if (mysqli_query($con, $sql)) {
+        echo "Record inserted successfully";
+    } else {
+        echo "Error: " . mysqli_error($con);
+    }
+
+    // Close the connection
+    mysqli_close($con);
+?>
